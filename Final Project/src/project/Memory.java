daf7628dd5ad55f7faa45607aef07dc8400cd90a
Nickmarmann14 <33103611+Nickmarmann14@users.noreply.github.com>
2018-04-25 12:50:59 -0400

    first part
    memory class

---
+++ Final Project/src/project/Memory.java
@@ -0,0 +1,19 @@
+package project;
+
+public class Memory {
+	
+	public static final int DATA_SIZE = 2048;
+	private int[] data = new int[DATA_SIZE];
+	
+	private int[] getAllData() {
+		return data;
+	}
+	
+	public int getData(int index) {
+		return data[index];
+	}
+	
+	public void setData(int index, int value) {
+		data[index] = value;
+	}
+}
