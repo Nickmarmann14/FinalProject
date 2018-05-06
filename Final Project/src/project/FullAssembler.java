3f60ae22c9a271da43618a929a01645d4447f136
Nickmarmann14 <33103611+Nickmarmann14@users.noreply.github.com>
2018-05-06 16:49:52 -0400

    IMPORTANT: Added JUnit4 to build path. WILL NOT WORK without this.

---
+++ Final Project/src/project/FullAssembler.java
@@ -0,0 +1,11 @@
+package project;
+
+public class FullAssembler implements Assembler {
+
+	@Override
+	public int assemble(String inputFileName, String outputFileName, StringBuilder error) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
