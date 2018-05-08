3f4f6ace3e4502c3ab4640822eb795a85afd5e19
Nickmarmann14 <33103611+Nickmarmann14@users.noreply.github.com>
2018-05-07 22:01:39 -0400

    Finished first part of error checking

--- Final Project/src/project/FullAssembler.java
+++ Final Project/src/project/FullAssembler.java
@@ -7,16 +7,59 @@ import java.util.Scanner;
 
 public class FullAssembler implements Assembler {
 
-	ArrayList<String> instructionsRead = new ArrayList<>();
-	
+	ArrayList<String> codeLines = new ArrayList<>();
+	ArrayList<String> dataLines = new ArrayList<>();
+	int lineCounter, errorLine = 0;
+	boolean isCode = true;
+	boolean blankFound = false;
+	String nextLine;
+
 	@Override
 	public int assemble(String inputFileName, String outputFileName, StringBuilder error) {
 		try(Scanner sc = new Scanner(new File(inputFileName))){
 			
-		} catch(Exception e) {
+			while(sc.hasNextLine()) { // This is the first pass which divides code/data and finds whole code errors. The next loop should discover errors with individual lines of code.
+				lineCounter++;
+				nextLine = sc.nextLine();
+				if(nextLine.trim().length()==0){
+					if(!(blankFound)) {
+						error.append("\nBlank Line Error on line"+ lineCounter); // Error 1
+						errorLine = lineCounter;
+					}
+					blankFound = true;
+				}
+				if(nextLine.charAt(0)==' ' || nextLine.charAt(0)=='\t') {
+					nextLine = nextLine.trim();
+					error.append("\nBlank Start Error on line "+lineCounter); // Error 2
+					errorLine = lineCounter;
+				}
+				if(isCode) {
+					if(nextLine.trim().equals("DATA")) {
+						if(nextLine.trim().toUpperCase().equals("DATA")) {
+							error.append("\n \"DATA\" must be uppercase! Line: "+lineCounter); // Part of 3
+							errorLine = lineCounter;
+						}
+						isCode = false;
+					}
+					else {
+						codeLines.add(nextLine);
+					}
+				} else {
+					if(nextLine.equals("DATA")) {
+						error.append("\nYou cannot have two lines with DATA: line "+lineCounter); // Error 3
+						errorLine = lineCounter;
+					} else {
+						dataLines.add(nextLine);
+					}
+				}
+			}
+			lineCounter = 0; // Resetting the line counter for the second pass. Be careful about the line number in the second pass. For example, account for the lack of "DATA" in the line count.
 			
+		} catch(FileNotFoundException e) {
+			error.append("\nUnable to open the source file");
+			return -1;
 		}
-		return 0;
+		return errorLine;
 	}
 
 }
